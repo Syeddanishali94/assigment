37d57908e4326f8bc2ffa4bcef76f4da15ce073b
Syeddanishali94 <syeddanishali06@gmail.com>
2024-12-30 11:04:27 -0800

    update

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ interface Post {
 
 const fetchPosts = async (): Promise<Post[]> => {
   try {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/posts`, { cache: 'no-store' });
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/posts`, { cache: 'no-store' });
 
     if (!response.ok) {
       throw new Error('Failed to fetch posts');
